9ef023aae333002de9c1cf21e640eccc77de2a04
ILARY-D <mamdjoilary@gmail.com>
2026-06-23 13:52:55 +0200

    mon deuxième commit sur le projet Azertype

--- azertype.js
+++ azertype.js
@@ -1,29 +1,45 @@
-const listeMots = [ "cachalot","petunia","serviette"];
-const listePhrases =["pas de panique !", "la vie, l'univers et le reste","merci pour le poisson "]
-let score = 0;
 
-let choix = prompt("veillez choisir la liste des: mots ou phrases ")
 
-while (choix !== "mots" && choix !== "phrases") {
-    choix = prompt("veillez choisir la liste des: mots ou phrases ")
+function affichetResultat( score, nbmotsproposés){
+    // afficher le score
+console.log("votre score est de : " + score+ " sur " + nbmotsproposés )
 }
+function choisirPhraseOuMot(){
+    // choisir les mots ou les phrases
+    let choix = prompt("veillez choisir la liste des: mots ou phrases ");
 
-if (choix === "mots") {
-    for(let i = 0; i < listeMots.length; i++){ 
-    let mottape = prompt("entrez le mot " + listeMots[i]);
-    if (mottape === listeMots[i]) {
-        score +=1;   
-    }
+    while (choix !== "mots" && choix !== "phrases") {
+    choix = prompt("veillez choisir la liste des: mots ou phrases ");
 }
-console.log("votre score est de : " + score+ " sur " +listeMots.length); 
-} else{
-    for(let i = 0; i < listePhrases.length; i++){ 
-      let mottape = prompt("entrez la phrase : " + listePhrases[i]);
-       if (mottape === listePhrases[i]) {
-        score +=1;   
+return choix;
+}
+function lancerBoucleDeJeu(listeProposition){
+    let score = 0;
+     for( let i = 0; i < listeProposition.length; i++){ 
+        let mottape = prompt ("entrez le mot :" +listeProposition[i])
+       if (  mottape === listeProposition[i] ) {
+score +=1;  
        }
-   
     }
-    console.log("votre score est de : " + score+ " sur " + listePhrases.length);
+    return score;
 }
+function lancerJeu(){
+    let choix = choisirPhraseOuMot();
+    let score = 0;
+    let nbmotsproposés;
+    
+
+    if (choix === "mots") {
+       
+       score = lancerBoucleDeJeu(listeMots)
+        nbmotsproposés = listeMots.length
+    } else{
+   
+        score = lancerBoucleDeJeu(listePhrases)
+        nbmotsproposés=listePhrases.length
+}
+   affichetResultat (score,  nbmotsproposés)
+}
+lancerJeu()
+
 
